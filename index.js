adc5c0bb88470db9c729735004f80d6b142590d9
Sadia Khan <khansadia220@gmail.com>
2024-01-11 10:50:00 +0100

    updated index.js where user name and date of birth is shown

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+// Asking name from user
 let Name = prompt("What is your name?");
 console.log("Hello " + Name + "!");
+ 
+// Asking ageyear from user 
+const AgeYear = prompt (" What is your age year?");
+ console.log("My age year is" + AgeYear + "!");
+
+ //Beräkna användarens ålder genom att subtrahera födelseåret från det aktuella året. 
+ 
+
+const BirthYear = prompt("What is your birth year?");
+const age = new Date().getFullYear() - BirthYear;
+console.log("You are " + age + " year old.");
