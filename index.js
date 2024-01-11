cf697e96f6c2a9eaf0881f5ce98ef6829a47f636
Sadia Khan <khansadia220@gmail.com>
2024-01-11 11:15:47 +0100

    Mini calculator is created

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Asking name from user
+//Asking name from user
 let Name = prompt("What is your name?");
 console.log("Hello " + Name + "!");
  
@@ -8,7 +8,22 @@ const AgeYear = prompt (" What is your age year?");
 
  //Beräkna användarens ålder genom att subtrahera födelseåret från det aktuella året. 
  
-
 const BirthYear = prompt("What is your birth year?");
 const age = new Date().getFullYear() - BirthYear;
-console.log("You are " + age + " year old.");
+console.log("You are " + age + " year old."); 
+
+
+// Now its time to create mini calculator.// starting with asking for two number using prompt.
+
+
+let nummer1 = prompt("Enter your first number:");
+let nummer2 = prompt("Enter your second number:");
+
+console.log(nummer1 + " + " + nummer2 + " = " + (Number(nummer1) + Number(nummer2)));
+console.log(nummer1 + " - " + nummer2 + " = " + (nummer1 - nummer2));
+console.log(nummer1 + " * " + nummer2 + " = " + (nummer1 * nummer2));
+console.log(nummer1 + " / " + nummer2 + " = " + (nummer1 / nummer2));
+// Tänk på att prompt ALLTID returnerar en sträng.
+let string = prompt("Enter a nummer:");
+let nummer = parseInt(string);
+alert("Beräkningarna är klara! Kontrollera konsolen för resultaten.");
